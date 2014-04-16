23d327ed26933e3a74ae280e8077287bec69cd4c
PaulHughes01 <PaulHughes01@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-04-16 07:20:13 +0000

    Release 2.0.14.
    git-svn-id: https://plugins.svn.wordpress.org/muut/tags/2.0.14@894115 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ moot.php
@@ -0,0 +1,27 @@
+<?php
+/*
+ * Plugin Name:   Muut
+ * Plugin URI:    https://muut.com/docs/wordpress.html
+ * Author URI:    https://muut.com
+ * Description:   Realtime forums and commenting for WordPress.
+ * Version:       2.0.14
+ * Author:        Muut Inc
+ * Text Domain:   muut
+ * License:       MIT
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) { die; }
+
+require_once( plugin_dir_path( __FILE__ ) . 'class-muut.php' );
+
+/*
+
+// Register hooks
+register_activation_hook( __FILE__, array( 'Muut', 'activate' ) );
+register_deactivation_hook( __FILE__, array( 'Muut', 'deactivate' ) );
+
+*/
+
+
+Muut::get_instance();
