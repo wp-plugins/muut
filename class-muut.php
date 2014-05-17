75e67096b645dc9e9b8610a336610fd3ef5ce12c
PaulHughes01 <PaulHughes01@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-05-17 16:15:38 +0000

    Release 2.0.16
    git-svn-id: https://plugins.svn.wordpress.org/muut/tags/2.0.16@916168 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ class-muut.php
@@ -0,0 +1,166 @@
+<?php
+/**
+ * The new-wave commenting and forums for Wordpress
+ *
+ * @package  Muut
+ * @author   Tero Piirainen <tero@muut.com>
+ * @license  MIT
+ * @link    https://muut.com/docs/wordpress.html
+ * @copyright 2014 Muut Inc
+ */
+
+class Muut {
+
+  protected $version = '2.0.16';
+
+  protected $plugin_slug = 'muut';
+
+  protected static $instance = null;
+
+  protected $plugin_screen_hook_suffix = null;
+
+
+  private function __construct() {
+
+	add_action( 'admin_init', array( $this, 'upgradeToMuut' ) );
+	add_action( 'init', array( $this, 'disregardOldMoot' ), 2 );
+
+    add_filter('the_content', array($this, 'default_comments'));
+
+    add_action('wp_enqueue_scripts', array($this, 'muut_includes'));
+    add_action('wp_head', array($this, 'muut_head'));
+    add_action('admin_menu', array($this, 'muut_admin_menu'));
+    add_action('admin_init', array($this, 'muut_settings'));
+
+	add_shortcode('muut', array($this, 'muut_shortcode'));
+	add_shortcode('no-muut', array($this, 'muut_disable'));
+
+	// Deprecated
+	add_shortcode('moot', array($this, 'muut_shortcode'));
+    add_shortcode('no-moot', array($this, 'muut_disable'));
+  }
+
+
+  public static function get_instance() {
+    if (null == self::$instance) { self::$instance = new self; }
+    return self::$instance;
+  }
+
+  public function disregardOldMoot() {
+	  if ( class_exists( 'Moot' ) ) {
+	  	remove_filter('the_content', array(Moot::get_instance(), 'default_comments'));
+
+	  	remove_action('wp_enqueue_scripts', array(Moot::get_instance(), 'moot_includes'));
+	  	remove_action('wp_head', array(Moot::get_instance(), 'moot_head'));
+	  	remove_action('admin_menu', array(Moot::get_instance(), 'moot_admin_menu'));
+	  	remove_action('admin_init', array(Moot::get_instance(), 'moot_settings'));
+	  }
+  }
+
+  public function upgradeToMuut() {
+	  $muut_options = get_option( 'muut_options', array() );
+	  if ( !isset( $muut_options['current_version'] ) || ( is_string( $muut_options['current_version'] ) && version_compare( $muut_options['current_version'], '2.0.13', '<' ) ) ) {
+		  update_option( 'muut_forum_name', get_option( 'moot_forum_name', '' ) );
+		  update_option( 'muut_api_key', get_option( 'moot_api_key', '' ) );
+		  update_option( 'muut_secret_key', get_option( 'moot_secret_key', '' ) );
+		  update_option( 'muut_language', get_option( 'moot_language', '' ) );
+		  update_option( 'muut_generate', get_option( 'moot_generate', '' ) );
+		  update_option( 'muut_comments_under_forums', get_option( 'moot_comments_under_forums', '' ) );
+
+		  delete_option( 'moot_forum_name' );
+		  delete_option( 'moot_api_key' );
+		  delete_option( 'moot_secret_key' );
+		  delete_option( 'moot_language' );
+		  delete_option( 'moot_generate' );
+		  delete_option( 'moot_comments_under_forums' );
+	  }
+
+	  if ( !isset( $muut_options['current_version'] ) || version_compare( $muut_options['current_version'], $this->version, '<' ) ) {
+		  update_option( 'muut_options', array( 'current_version' => $this->version ) );
+	  }
+  }
+
+  public function muut_head() {
+    require_once(plugin_dir_path(__FILE__) . 'public.php');
+  }
+
+  public function muut_includes() {
+
+    if (!is_home()) {
+      wp_enqueue_style("moot-wordpress", '//cdn.muut.com/latest/moot.css', array(), $this->version);
+
+      $lang = get_option('muut_language');
+      if ($lang == 'en') $lang = "";
+      if ($lang) $lang = "." . $lang;
+
+      wp_enqueue_script("", "//cdn.muut.com/latest/moot$lang.min.js", array('jquery'), $this->version);
+    }
+  }
+
+  public function default_comments($content) {
+    $forumname = get_option('muut_forum_name');
+
+	if (!is_home() && $forumname != null && get_option('muut_generate') == "true" && in_array( get_post_type(), array( 'post', 'page' ) ) ) {
+      $page_id = sanitize_title(get_the_title());
+      $content .= "<a id='moot-default-comments' href='https://moot.it/i/$forumname/wordpress:$page_id'>Comments</a>";
+    }
+
+    return $content;
+  }
+
+  public function muut_settings($content) {
+    register_setting('muut_options', 'muut_forum_name');
+    register_setting('muut_options', 'muut_api_key');
+    register_setting('muut_options', 'muut_secret_key');
+    register_setting('muut_options', 'muut_language');
+    register_setting('muut_options', 'muut_generate');
+    register_setting('muut_options', 'muut_comments_under_forums');
+  }
+
+  // admin menu
+  public function muut_admin_menu() {
+
+    if (is_super_admin()) {
+      $this->plugin_screen_hook_suffix = add_plugins_page(
+        __('Muut', $this->plugin_slug),
+        __('Muut', $this->plugin_slug),
+        'read', $this->plugin_slug, array($this, 'muut_admin')
+      );
+    }
+
+  }
+
+  public function muut_admin() {
+    include_once('settings.php');
+  }
+
+  public function muut_disable() {
+    return "<span id='no-moot'></span>";
+  }
+
+  public function muut_shortcode($params) {
+
+    extract( shortcode_atts( array(
+      'forum' => false,
+      'threaded' => false,
+      'path' => false
+
+    ), $params) );
+
+    $forumname = get_option('muut_forum_name');
+
+    if ($forumname == null) return "";
+
+    $tag = "<a id='moot' href='https://moot.it/i/$forumname";
+    $page_id = sanitize_title(get_the_title());
+
+
+    // (bool ? this : that) not working
+    if ($forum)   return "$tag'>$forumname forums</a>";
+    if ($threaded) return "$tag/wordpress/$page_id'>Comments</a>";
+    if ($path)    return "$tag/$path'>Comments are here</a>";
+              return "$tag/wordpress:$page_id'>Comments</a>";
+
+  }
+
+}
---
+++ public.php
@@ -0,0 +1,120 @@
+
+<!-- Use Moot instead of Wordpress for comments | https://muut.com */ -->
+<style>
+  .has-moot .comments-link,
+  .has-moot #comment-wrap,
+  .has-moot #comments,
+  .has-moot #recent-comments-2,
+  .has-moot a[href*=comments-rss],
+  .has-moot a[href*='comments/feed'],
+  .home-page #moot {
+    display: none !important;
+  }
+  .moot { max-width: 100%; margin-bottom: 2em; }
+  .moot p { margin-bottom: 0; }
+  .moot .m-title { line-height: 1.1; margin: 0; }
+  .moot .m-pagetitle { margin: 0 0 .6em; }
+</style>
+
+
+<?php
+
+  $key = get_option('muut_api_key');
+
+  // Single Sign-On enabled
+  if ($key) {
+    $timestamp = time();
+
+    $user_info = wp_get_current_user();
+
+    $display_name = $user_info->user_firstname ?
+      $user_info->user_firstname . " " . $user_info->user_lastname :
+      $user_info->display_name
+    ;
+
+    // userpro (bit.ly/1ckc3es) stores avatar URL to user_meta
+    $avatar = get_user_meta($user_info->ID, 'profilepicture', true);
+
+    if (!$avatar) {
+      $avatar = function_exists('bp_core_fetch_avatar') ?
+        bp_core_fetch_avatar(array(item_id => $user_info->ID, 'html' => false)) :
+        "//gravatar.com/avatar/" . md5($user_info->user_email)
+      ;
+    }
+
+    $sso = array(
+      "user" => array(
+        "id" => $user_info->user_login,
+        "email" => $user_info->user_email,
+        "avatar" => $avatar,
+        "displayname" => $display_name,
+        "is_admin" => is_super_admin()
+      )
+    );
+
+    // echo json_encode($sso);
+
+    $message = base64_encode(json_encode($sso));
+    $signature = sha1(get_option('muut_secret_key') . ' ' . $message . ' ' . $timestamp);
+  }
+
+?>
+
+<script>
+!function($) {
+
+$(function() {
+
+  <?php if (get_option('muut_generate') == "true") { ?>
+    $("body").addClass("has-moot");
+  <?php } ?>
+
+  if (typeof moot != "function") return $("#moot").remove();
+
+  <?php if (get_option('muut_comments_under_forums')) { ?>
+    moot(function(app) {
+      if (app.is_forum) {
+        $(".m-forums").append('<p><a href="#!/wordpress">Comments</a></p>');
+      }
+    })
+  <?php } ?>
+
+  <?php if ($key) { ?>
+    var moot_conf = {
+      login_url: '<?php echo wp_login_url(get_permalink()); ?>',
+      sso: {
+        key: '<?php echo $key; ?>',
+        timestamp: <?php echo $timestamp; ?>,
+        signature: '<?php echo $signature; ?>',
+        message: '<?php echo $message; ?>'
+      }
+    }
+  <?php } else { ?>
+    var moot_conf = {};
+
+  <?php } ?>
+
+  var default_moots = $("a[id='moot-default-comments']"),
+    user_moot = $("#moot");
+
+  if ($("#no-moot")[0]) return default_moots.remove();
+
+  // no support multiple instances yet
+  if (default_moots[1]) {
+    default_moots.remove();
+
+  } else if (user_moot[0]) {
+    user_moot.moot(moot_conf);
+    default_moots.remove();
+
+  } else {
+    default_moots.moot(moot_conf);
+  }
+
+  if (moot()) $("body").addClass("has-moot");
+
+})
+
+}(jQuery)
+
+</script>
